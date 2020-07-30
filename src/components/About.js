fe4156d9c850d6b13c18e5c883fb11e226692335
Kyle Petersen <60599938+kpete2017@users.noreply.github.com>
2020-07-30 13:56:30 -0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Fade from 'react-reveal/Fade';
 import './About.css'
+import rl1 from '../assets/rl1.mp4'
+import rl2 from '../assets/rl2.mp4'
+import rl3 from '../assets/rl3.mp4'
 
 export default class about extends React.Component {
 
@@ -19,7 +22,20 @@ export default class about extends React.Component {
                     <br/>
                     Headquartered in Colorado, Zelos is proud to represent the North American region as we compete against our international rivals. As esports continues to rise into the mainstream sports world, Zelos is working hard to innovate and provide fans with top of the line rosters, media content, and streetwear under an influential and compelling brand.
                 </p>
-                <h2 onClick={this.handleLearnMoreClick}>LEARN MORE</h2>
+                <video width="320" height="240" autoplay="true" muted="true" id="rl1" loop="true">
+                    <source src={rl1} type="video/mp4"/>
+                    Your browser does not support the video tag.
+                </video>
+
+                <video width="320" height="240" autoplay="true" muted="true" id="rl2" loop="true">
+                    <source src={rl2} type="video/mp4"/>
+                    Your browser does not support the video tag.
+                </video>
+                <video width="320" height="240" autoplay="true" muted="true" id="rl3" loop="true">
+                    <source src={rl3} type="video/mp4"/>
+                    Your browser does not support the video tag.
+                </video>
+                <h2 onClick={ () => this.props.aboutPage()}>LEARN MORE</h2>
                 </Fade>
             </div>
         )
