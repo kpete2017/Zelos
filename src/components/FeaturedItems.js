8ffb3da06f63f409bcf8676df1096f6496fadbee
Kyle Petersen <60599938+kpete2017@users.noreply.github.com>
2020-07-31 01:53:05 -0600

    Update FeaturedItems.js

--- src/components/FeaturedItems.js
+++ src/components/FeaturedItems.js
@@ -13,20 +13,25 @@ export default function FeaturedItems() {
     return(
         <Fade>
             <div className="store-section">
+                <div className="black-background">
                 <h1 id="team-speak">Become part of the team</h1>
                 <div className="featured-items">
-                    <div className="item">
+                    <div className="featured-item">
                         <img id="tee-1" height="200" width="400"src={tee1} alt="Zelos Esports"></img>
                         <h4 className="item-desc">Zelos Long Sleeve</h4>
+                        <h4 className="purchase-button">Purchase</h4>
                     </div>
-                    <div className="item">
+                    <div className="featured-item">
                         <img id="tee-2" height="200" width="400"src={tee2} alt="Zelos Esports"></img>
                         <h4 className="item-desc">Poseidon Collection 2020 Jersey</h4>
+                        <h4 className="purchase-button">Purchase</h4>
                     </div>
-                    <div className="item">
+                    <div className="featured-item">
                         <img id="tee-3" height="200" width="400"src={tee3} alt="Zelos Esports"></img>
                         <h4 className="item-desc">Zelos Windbreaker</h4>
+                        <h4 className="purchase-button">Purchase</h4>
                     </div>
+                </div>
                 </div>
             </div>
         </Fade>
