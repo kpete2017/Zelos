6323aa235be5d40da56b9943f6ebfd22a14cf5b0
Kyle Petersen <60599938+kpete2017@users.noreply.github.com>
2020-07-31 01:59:43 -0600

    Update Header-Content.js

--- src/containers/Header-Content.js
+++ src/containers/Header-Content.js
@@ -2,23 +2,15 @@ import React from 'react'
 import logo from '../assets/Zelos7_3.png'
 import insta from '../assets/insta.png'
 import twitter from '../assets/twitter.png'
+import twitch from '../assets/twitch.png'
 import './Header.css'
 
 export default class HeaderContent extends React.Component {
-
-    handleOnInstaClick = () => {
-        window.open('https://www.instagram.com/zeloscompete/')
-    }
-
-    handleOnTwitterClick = () => {
-        window.open('https://twitter.com/ZelosCompete')
-    }
-
     render() {
         return(
             <header>
                 <div className="title-card">
-                    <img src={logo} width="200" height="50" alt="Zelos Esports" onClick={() => this.props.titlePage()}></img>
+                    <img id="zelos-header-logo" src={logo} width="200" height="50" alt="Zelos Esports" onClick={() => this.props.titlePage()}></img>
                 </div>
                 <div className="nav">
                     <h4 id="nav-item" onClick={() => this.props.titlePage()}>HOME</h4>
@@ -27,8 +19,9 @@ export default class HeaderContent extends React.Component {
                     <h4 id="nav-item" onClick={() => this.props.storePage()}>STORE</h4>
                     <h4 id="nav-item" onClick={() => this.props.teamPage()}>TEAMS</h4>
                     <div className="plugs">
-                        <img src={insta} width="35" height="35" alt="Zelos Esports" onClick={this.handleOnInstaClick} id="insta"></img>
-                        <img src={twitter} width="35" height="35" alt="Zelos Esports" onClick={this.handleOnTwitterClick} id="twitter"></img>
+                        <img src={insta} width="35" height="35" alt="Zelos Esports" onClick={() => window.open("https://www.instagram.com/zeloscompete/")} id="insta"></img>
+                        <img src={twitter} width="35" height="35" alt="Zelos Esports" onClick={() => window.open("https://twitter.com/ZelosCompete")} id="twitter"></img>
+                        <img src={twitch} width="35" height="35" alt="Zelos Esports" id="twitch" onClick={() => window.open("https://www.twitch.tv/zeloscompete")}></img>
                     </div>
                 </div>
             </header>
