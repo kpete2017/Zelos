79840f59aff700113d953d226cbea3013e17225d
Kyle <kpete2017@gmail.com>
2020-08-24 20:11:21 -0600

    Fixed google firebase bug

--- src/only_open_this_folder_to_edit/event1.js
+++ src/only_open_this_folder_to_edit/event1.js
@@ -5,9 +5,13 @@ import comingSoon from '../assets/ZEL-Placeholder-schedule.png'
 export default function Event1() {
     return(
         <div className="events">
-            <p>Pre Season League</p>
-            <p>TBD</p>
-            <p>Tom Clancy's Rainbow Six Siege</p>
+            {/* This is for the title of the event */}
+            <p id="Event-Title">Pre Season League</p>
+            {/* This is for the date of the event */}
+            <p id="Time-of-Event">TBD</p>
+            {/* This is the Game being played */}
+            <p id="Name-of-Game">Tom Clancy's Rainbow Six Siege</p>
+            {/* This is the home and vs logos */}
             <div className="vs">
                 <img id="zelos-home-logo" height="75" width="120"src={HomeLogo} alt="Zelos Esports"></img>
                 <h2>VS</h2>
